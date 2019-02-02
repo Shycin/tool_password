048f00d199bd3fa39533ac5f9e93efe410dd654c
Shycin <43355952+Shycin@users.noreply.github.com>
2019-02-02 13:33:36 +0100

    Update tool_password.js

--- tool_password.js
+++ tool_password.js
@@ -6,15 +6,43 @@ $(document).ready(function() {
 		"</style>"+
 	"");
 
-	var handler_password = function()
+	function findPos(obj){
+
+	    var curleft = 0;
+	    var static = 1;
+
+	    if (obj.offsetParent) 
+	    {
+	        curleft = obj.offsetLeft
+
+	        while (obj = obj.offsetParent) 
+	        {
+	        	if(static)
+	        	{
+	        		if($(obj).css("position") != "static")
+	        		{
+	        			static = 0;
+	        		}
+
+	        		curleft += obj.offsetLeft
+	        	}
+	            
+	        }
+	    }
+	    return curleft;
+	}
+
+	function handler_password()
 	{
 		var position = $(this).parent().find($("input:password"));
 
 
-		var start = position.position().left;
+		var start = findPos($(position).get(0));
 		var taille = position.width();
 
+
 		var calcul = start + taille - position.height(); //calcul distance élément initial + sa longueur - la taille que va faire le nouvelle élément
+
 
 		$(this).css({
 	      	"left": calcul,
@@ -25,8 +53,33 @@ $(document).ready(function() {
 
 
 	$("input:password").parent().append("<img class='tool_show_password' src='https://raw.githubusercontent.com/Shycin/tool_password/master/show_password.png'>");
-	$(".tool_show_password").on( "load", handler_password );
 
+	$(".tool_show_password").on( "load" , handler_password );
+
+	$(window).resize(function(){
+
+		$("input:password").each(function() {
+
+			var position = $(this);
+			var element = $(this).parent().find($("img.tool_show_password"));
+
+			var start = findPos($(position).get(0));
+			var taille = position.width();
+
+
+			var calcul = start + taille - position.height(); //calcul distance élément initial + sa longueur - la taille que va faire le nouvelle élément
+
+
+			$(element).css({
+		      	"left": calcul,
+		      	"height": position.height(),
+		      	"width": position.height(),
+		    });
+		});
+
+	});
+
+	
 
 	$( "input:password" ).parent().find(".tool_show_password")
 	.on("mousedown",function(){
